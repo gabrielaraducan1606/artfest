e6a85c1350ffec15aea0f256aed2a7a6c1b12e6f
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2025-10-25 10:01:18 +0300

    commit9

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import AuthProvider from "./pages/Auth/Context/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
 import "./styles/variables.css";
 
 const root = createRoot(document.getElementById("root"));
+const isProd = import.meta.env.PROD;
 
-const isProd = import.meta.env.PROD; // Vite setează PROD în build
 const tree = (
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <HelmetProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </HelmetProvider>
 );
 
-root.render(isProd ? tree : tree); // fără StrictMode în dev
+root.render(isProd ? tree : tree);
