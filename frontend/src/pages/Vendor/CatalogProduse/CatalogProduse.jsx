169289736ec46c322519aa77ea22ca664de54b12
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2026-08-26 18:52:25 +0300

    cc

--- frontend/src/pages/Vendor/CatalogProduse/CatalogProduse.jsx
+++ frontend/src/pages/Vendor/CatalogProduse/CatalogProduse.jsx
@@ -12,7 +12,7 @@ import {
 import styles from "./CatalogProduse.module.css";
 import CatalogImports from "./imports/CatalogImports.jsx";
 import ProductModal from "../ProfilMagazin/modals/ProductModal.jsx";
-import CampaignsTab from "./campaigns/CampaignsTab.jsx";
+import CampaignsTab from "./Campaigns/CampaignsTab.jsx";
 
 import {
   useAnnounceCurrentEntity,
